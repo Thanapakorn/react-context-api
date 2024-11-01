6fb639f52f1c096569eeaf5ac66ba9352df35b2b
thanapakorn <thanapakorn001@gmail.com>
2024-11-01 14:30:25 +0700

    feat:add answer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+import { createContext } from "react";
 
+export const Usercontext = createContext({});
 function App() {
   const userData = {
     username: "John",
@@ -12,14 +14,16 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <Usercontext.Provider value={{ userData }}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </Usercontext.Provider>
   );
 }
 
--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,12 @@
+import { Usercontext } from "../App";
+import { useContext } from "react";
+
 function AppHeader() {
+  const { userData } = useContext(Usercontext);
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {userData.username}</h2>
     </div>
   );
 }
